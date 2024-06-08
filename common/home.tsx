abfe1466299986c5dccf5b0ace357d84cb803799
Tobias Westphal <hey@tobiaswestphal.me>
2024-06-08 19:12:36 +0200

    Some fixes

--- common/home.tsx
+++ common/home.tsx
@@ -1,14 +1,11 @@
 export default function App() {
   return (
     <main class="w-screen h-screen relative text-white">
-      <div
-        class="absolute w-full"
-        style="padding: 20px 48px; background: #3d3b47;"
-      >
-        <img src="./rsc/logo_white.svg" alt="Logo" style="height: 48px;"></img>
+      <div class="absolute w-full bg-[#1f1f26] py-8 px-12">
+        <img src="./rsc/logo_white.svg" alt="Logo" class="h-12"></img>
       </div>
-      <div class="w-full h-full md:flex md:flex-row">
-        <div class="md:h-full w-full md:w-1/2 lg:w-2/3 flex flex-col place-content-center place-items-center gap-6 p-12">
+      <div class="w-full h-full md:flex md:flex-row pt-32">
+        <div class="md:h-full w-full md:w-1/2 flex flex-col place-content-center place-items-center gap-6 p-12">
           <h1
             class="w-full font-semibold text-rose-200 leading-tight"
             style="font-size: 3em;"
@@ -16,10 +13,13 @@ export default function App() {
             Like YouTube<br></br>but
             <span class="text-rose-500 font-bold"> with friends.</span>
           </h1>
-          <p class="w-full text-rose-200 text-lg">
-            Take the hassle out of starting a watch party. Just let your friends
-            vote for the next song and have your party run on autopilot.
-          </p>
+          <div class="w-full">
+            <p class="max-w-[34rem] text-rose-200 text-lg">
+              Take the hassle out of starting a watch party. Just let your
+              friends vote for the next song and have your party run on
+              autopilot.
+            </p>
+          </div>
           <div class="min-w-full">
             <button class="bg-rose-500 hover:bg-rose-600 w-fit text-white font-medium gap-2 rounded-full font-lg py-3 px-6 flex items-center">
               <svg
@@ -38,11 +38,8 @@ export default function App() {
             </button>
           </div>
         </div>
-        <div class="md:h-full w-full md:w-1/2 lg:w-1/3 flex items-center justify-center">
-          <div
-            class="absolute w-80 h-80 bg-rose-300 rounded-full"
-            style="z-index: -5"
-          ></div>
+        <div class="md:h-full w-full md:w-1/2 flex items-center justify-center pt-12 md:pt-0">
+          <div class="absolute w-80 h-80 bg-rose-300 rounded-full z-[-5]"></div>
           <img
             class="w-56 border-4 border-rose-500 rounded-lg flex justify-center rotate-6"
             /* TO-DO: Change mockup once final client user interface is ready */

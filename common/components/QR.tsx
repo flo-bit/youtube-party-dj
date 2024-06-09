7fe5388d7397e897afbce0d0ea17920d42e0059d
Elena Pervova <cornflower20189@gmail.com>
2024-06-09 11:01:47 +0200

    Fix qr code

--- common/components/QR.tsx
+++ common/components/QR.tsx
@@ -1,23 +1,12 @@
-import QRCodeStyling from "https://esm.sh/qr-code-styling@1.6.0-rc.1"
+export default function QRCode({ url }) {
+  const qrCodeSrc = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(url)}&format=svg`
 
-export default function QRCode() {
-  const qrCode = new QRCodeStyling({
-    width: 256,
-    height: 256,
-    data: "google.com"
-  })
-
-  setTimeout(() => {
-    const target = document.getElementById("qr-code-target")
-    console.log(target)
-    // if target is empty, append it
-    if (target?.children.length === 0) {
-      qrCode.append(document.getElementById("qr-code-target"))
-    }
-  }, 0)
- return (
-    <div class="aspect-square w-3/4 md:w-full bg-white/5 border border-white/10 rounded-xl z-10">
-      <div id="qr-code-target" class="w-full h-full flex items-center justify-center text-white">
+  return (
+    <div className="aspect-square w-3/4 md:w-full bg-white/5 border border-white/10 rounded-xl z-10">
+      <div id="qr-code-target" className="w-full h-full flex items-center justify-center text-white">
+        <img className="w-full h-full" src={qrCodeSrc} alt="QR code" />
       </div>
-    </div>);
+    </div>
+  );
 }
+

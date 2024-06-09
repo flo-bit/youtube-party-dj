b270e4ac186fc793af24db9e0b7fdadb8317b62f
Florian <45694132+flo-bit@users.noreply.github.com>
2024-06-09 15:47:55 +0200

    change className to class

--- common/components/QR.tsx
+++ common/components/QR.tsx
@@ -2,9 +2,9 @@ export default function QRCode({ url }) {
   const qrCodeSrc = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(url)}&format=svg`
 
   return (
-    <div className="aspect-square w-3/4 md:w-full bg-white/5 border border-white/10 rounded-xl z-10">
-      <div id="qr-code-target" className="w-full h-full flex items-center justify-center text-white">
-        <img className="w-full h-full" src={qrCodeSrc} alt="QR code" />
+    <div class="aspect-square w-3/4 md:w-full border border-white/10 rounded-xl z-10 overflow-hidden bg-white p-4">
+      <div id="qr-code-target" class="w-full h-full flex items-center justify-center text-white">
+        <img class="w-full h-full" src={qrCodeSrc} alt="QR code" />
       </div>
     </div>
   );

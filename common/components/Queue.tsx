93ef3fb7eb1c19f598a02f86c32d5b39669e6de0
moritzbarv <149739890+moritzbarv@users.noreply.github.com>
2024-06-23 16:27:08 +0200

    Update Queue.tsx

--- common/components/Queue.tsx
+++ common/components/Queue.tsx
@@ -1,7 +1,7 @@
 import { QueueItem } from "./QueueItem.tsx";
 import { Item } from "backend/sessions.ts";
 
-export type QueueType = 'player' | 'client' | 'search';
+export type QueueType = 'player' | 'client' | 'search' | 'history' ;
 
 export function Queue({ items, type, code }: Readonly<{ items: Item[], type: QueueType, code: string }>) {
   return <div class="space-y-4">{

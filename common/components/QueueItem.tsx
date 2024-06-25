2da1d1f1d448761328f05cf9ac3e633b0e37dff8
yunusbeyaz <yunus.noah.beyaz@gmail.com>
2024-06-25 17:28:25 +0200

    simple like-animation & check-hooks
    Removed notification.
    The like-button now has a small animation when clicked.
    Check-hooks appear now in the search when a video is added, so no video can be added twice.

--- common/components/QueueItem.tsx
+++ common/components/QueueItem.tsx
@@ -1,38 +1,6 @@
 import { ObjectRef } from "unyt_core/runtime/pointers.ts";
 import { QueueType } from "./Queue.tsx";
-import { getSessionWithCode, getUserId, Item, toggleLike, addLike} from "backend/sessions.ts";
-
-let currentNotification = null;
-
-function showErrorNotification(message) {
-    
-    if (currentNotification) {
-        currentNotification.remove();
-    }
-
-    const notification = document.createElement('div');
-    notification.classList.add( 'bg-orange-100','border-r-4','border-l-4', 'border-orange-500', 'text-orange-700', 'p-6', 'fixed', 'top-0', 'right-4','left-4');
-
-    const boldText = document.createElement('p');
-    boldText.classList.add('font-bold');
-    boldText.textContent = " This video is already in the queue or currently playing: "
-    const videoTitel = document.createElement('p');
-    videoTitel.textContent = message;
-
-    notification.appendChild(boldText);
-    notification.appendChild(videoTitel);
-
-    document.body.appendChild(notification);
-
-    currentNotification = notification;
-
-    setTimeout(() => {
-        notification.remove();
-        currentNotification = null;
-    }, 2800);
-}
-
-
+import { getSessionWithCode, getUserId, Item, toggleLike} from "backend/sessions.ts";
 
 export async function QueueItem({
   item,
@@ -41,6 +9,69 @@ export async function QueueItem({
 }: Readonly<{ item: ObjectRef<Item>; type: QueueType; code: string }>) {
   const userId = (await getUserId()).userId;
 
+  async function renderIcon() {
+    const session = await getSessionWithCode(code);
+    if (!session) return null;
+
+    if (session.queue.some((v) => v.id == item.id) || session.currentlyPlaying?.id == item.id) {
+      return (
+        <div class="text-black dark:text-green-500 flex items-center justify-center">
+          <svg
+            viewBox="0 0 24 24"
+            class="size-6 items-center justify-center"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M5 12l5 5L20 7"
+            />
+          </svg>
+        </div>
+      );
+    } else {
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          class="size-6 dark:stroke-white stroke-black queueicon2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 4.5v15m7.5-7.5h-15"
+          />
+        </svg>
+      );
+    }
+  }
+  async function handleButtonClick() {
+    const session = await getSessionWithCode(code);
+    if (!session) {
+      return;
+    }
+    const button = document.getElementById(`button-${item.id}`);
+    if (button) {
+      button.innerHTML = `
+        <div class="text-black dark:text-green-500 flex items-center justify-center">
+          <svg viewBox="0 0 24 24" class="size-6 items-center justify-center">
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M5 12l5 5L20 7"
+            />
+          </svg>
+        </div>
+      `;
+    }
+  }
 
   function getAction() {
     if (type === "player") {
@@ -73,7 +104,7 @@ export async function QueueItem({
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
-                    class="size-6 ml-1"
+                    class="size-6 ml-1 bounce-animation"
                   >
                     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                   </svg>
@@ -104,49 +135,24 @@ export async function QueueItem({
     } else if (type === "search") {
       return (
         <button
-          onclick={async () => {
+          onclick= {async () => {
+            handleButtonClick();
             const session = await getSessionWithCode(code);
             if (!session) { // check if session is null
               return;
             }
 
-            // check if queue already contains the video
-            if (session.queue.some((v) => v.id == item.id) || session.currentlyPlaying.id == item.id) {
-              const button = document.getElementById(`button-${item.id}`);
-              addLike(code, item.id);
-              showErrorNotification("\""+item.title +'\".'); //optional Feature: Still want to add this video to the queue?
-
-              // Automatically like the video when added to the queue if not already liked
-              
-              button.classList.remove( 'border-black', 'dark:border-white/10');
-              button.classList.add('shake', 'border-red-500', 'dark:border-red-500');
-              setTimeout(() => {
-                button.classList.remove('shake', 'border-red-500', 'dark:border-red-500');
-                button.classList.add( 'border-black', 'dark:border-white/10');
-              }, 500);
+            if ((session.queue.some((v) => v.id == item.id) || session.currentlyPlaying?.id == item.id) ) {
+              //addLike(code, item.id);
               return;
             }
-            document.getElementById(`button-${item.id}`).style.display = 'none';
-            document.getElementById(`check-${item.id}`).style.display = 'flex';
             session?.$.queue.val.push(item);
             toggleLike(code, item.id);
           }}
           id={`button-${item.id}`}
           class="queueframe bg-white dark:bg-white/5 border border-black dark:border-white/10 rounded-full w-10 h-10 flex items-center justify-center"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            class="size-6 dark:stroke-white stroke-black queueicon2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
-          </svg>
+          {await renderIcon()}
         </button>
       );
     }
@@ -163,21 +169,7 @@ export async function QueueItem({
           </div>
           <div class="queueicon2 flex h-full justify-center items-center stroke-black dark:stroke-white px-2">
             {getAction()}
-            <div id={`check-${item.id}`} class="hidden dark:bg-white/5 bg-white border border-black dark:border-white/10 rounded-full w-10 h-10 items-center justify-center">
-              <svg
-                viewBox="0 0 24 24"
-                class="size-6 items-center justify-center"
-              >
-                <path
-                  fill="none"
-                  stroke="black dark:white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M5 12l5 5L20 7"
-                />
-              </svg>
-            </div>
+            
           </div>
         </div>
       </div>

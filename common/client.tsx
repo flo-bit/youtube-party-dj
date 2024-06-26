a6e7a2cd65a95bf27039a42587405939b84f417d
moritzbarv <149739890+moritzbarv@users.noreply.github.com>
2024-06-26 13:45:46 +0200

    Update client.tsx

--- common/client.tsx
+++ common/client.tsx
@@ -7,6 +7,8 @@ import { Context } from "uix/routing/context.ts";
 import { loadInitialTheme } from "./components/ToggleThemeButton.tsx";
 import NavMenu from "common/components/nav/NavMenu.tsx";
 
+export const playedVideos :Item[] = $$([]);
+
 export default async function App(ctx: Context) {
 	const code = (ctx.urlPattern?.pathname.groups[0] ?? "XXXX");
 
@@ -74,6 +76,10 @@ export default async function App(ctx: Context) {
 				<div class="px-4 py-4 border-t border-black dark:border-white/20 mx-0 overflow-y-scroll flex-grow">
 					{view}
 				</div>
+				<button onclick={()=>toggleHistory.val = !toggleHistory.val} class="absolute bottom-4 right-2 w-20 h-20 bg-white dark:bg-white/2 border border-black dark:border-white/10 rounded-full flex items-center justify-center backdrop-blur-md">
+                    		PLAYING HISTORY
+                		</button>
+				{toggle(toggleHistory, <Queue items={playedVideos} type={'history'} code={code}/>)}
 			</div>
 			{menu}
 		</main>

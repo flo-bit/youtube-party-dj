614f7edd55b9f69517120eea0bcc056274d21a66
Tobias Westphal <hey@tobiaswestphal.me>
2024-06-27 21:59:15 +0200

    Show how long the queue will last

--- common/page.tsx
+++ common/page.tsx
@@ -4,52 +4,93 @@ import { Queue } from "./components/Queue.tsx";
 import QRCodeOverlay from "./components/QRCodeOverlay.tsx";
 import { getSessionUserHosts } from "backend/sessions.ts";
 import { NowPlaying } from "./components/NowPlaying.tsx";
-import ToggleThemeButton, { loadInitialTheme } from "./components/ToggleThemeButton.tsx";
+import ToggleThemeButton, {
+  loadInitialTheme,
+} from "./components/ToggleThemeButton.tsx";
 
 export default async function App() {
-	const session = await getSessionUserHosts()
+  const session = await getSessionUserHosts();
 
-	const code = $$(session.code as string);
+  const code = $$(session.code as string);
 
-	const current = always(() => {
-		if (session.currentlyPlaying) {
-			return <div class="px-4 mx-0">
-			<div class="text-accent-500 font-semibold text-sm mb-2">currently playing</div>
-			<NowPlaying item={session.currentlyPlaying.$} />
-		</div>
-		} else {
-			return null;
-		}
-	})
+  const current = always(() => {
+    if (session.currentlyPlaying) {
+      return (
+        <div class="px-4 mx-0">
+          <div class="text-accent-500 font-semibold text-sm mb-2">
+            currently playing
+          </div>
+          <NowPlaying item={session.currentlyPlaying.$} />
+        </div>
+      );
+    } else {
+      return null;
+    }
+  });
 
-	loadInitialTheme();
+  function addDurations(duration1, duration2) {
+    let duration1Partitions = duration1.split(":").map(Number);
+    let duration2Partitions = duration2.split(":").map(Number);
+    let duration1Seconds = duration1Partitions[0] * 60 + duration1Partitions[1];
+    let duration2Seconds = duration2Partitions[0] * 60 + duration2Partitions[1];
+    let totalSeconds = duration1Seconds + duration2Seconds;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    return `${minutes}:${seconds}`;
+  }
 
-	return (
-		<main class="w-screen h-screen relative bg-gray-50 dark:bg-gray-950">
-			<div class="mx-auto grid md:grid-cols-2 h-screen">
-				<div class="h-screen hidden md:flex items-center flex-col justify-center p-8">
-					<QRCode code={code} />
-					<div class="text-black dark:text-white text-3xl font-semibold mt-4">Party code: <span>{code}</span></div>
-				</div>
-				<div
-					class="flex flex-col overflow-y-hidden h-screen bg-white dark:bg-white/5 border border-black dark:border-white/10 rounded-xl"
-				>
-					<div class="flex px-8 mx-0 mt-8 mb-4">
-						{/* @ts-ignore - uix doesn't support types? */}
-						<VideoPlayer queue={session.queue} code={code} />
-					</div>
-					<div class="flex items-center justify-end px-12 h-10 mb-4">
-						<ToggleThemeButton />
-					</div>
+  const timeLeft = always(() => {
+    let timeCounter = "0:00";
+    session.queue.forEach((item) => {
+      timeCounter = addDurations(timeCounter, item.duration);
+    });
+    return timeCounter;
+  });
 
-					{current}
-					<div
-						class="px-4 py-4 border-t border-black dark:border-white/20 mx-0 overflow-y-scroll flex-grow">
-						<Queue items={session.$.queue} type={'player'} code={code} />
-					</div>
-				</div>
-			</div>
+  loadInitialTheme();
 
-			<QRCodeOverlay code={code} />
-		</main>);
+  window.timeLeft = timeLeft;
+
+  return (
+    <main class="w-screen h-screen relative bg-gray-50 dark:bg-gray-950">
+      <div class="mx-auto grid md:grid-cols-2 h-screen">
+        <div class="h-screen hidden md:flex items-center flex-col justify-center p-8">
+          <QRCode code={code} />
+          <div class="text-black dark:text-white text-3xl font-semibold mt-4">
+            Party code: <span>{code}</span>
+          </div>
+        </div>
+        <div class="flex flex-col overflow-y-hidden h-screen bg-white dark:bg-white/5 border border-black dark:border-white/10 rounded-xl">
+          <div class="flex px-8 mx-0 mt-8 mb-4">
+            {/* @ts-ignore - uix doesn't support types? */}
+            <VideoPlayer queue={session.queue} code={code} />
+          </div>
+          <div class="flex items-center justify-end px-12 h-10 mb-4">
+            <ToggleThemeButton />
+          </div>
+
+          {current}
+          <div class="px-4 py-4 border-t border-black dark:border-white/20 mx-0 overflow-y-scroll flex-grow">
+            {always(() =>
+              timeLeft.val !== "0:00" ? (
+                <p class="mb-4"> Queue will last for {timeLeft} </p>
+              ) : (
+                <div>
+                  <p class="mb-2 font-bold">The queue is empty right now.</p>
+                  <p>
+                    Be the first one to add a song! Just scan the code on the
+                    left.
+                  </p>
+                </div>
+              )
+            )}
+            <Queue items={session.$.queue} type={"player"} code={code} />
+          </div>
+        </div>
+      </div>
+
+      <QRCodeOverlay code={code} />
+    </main>
+  );
 }

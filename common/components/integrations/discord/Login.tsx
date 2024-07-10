23c6660929183f3a71dadff73738d42d346246be
thaleius <58767350+thaleius@users.noreply.github.com>
2024-07-10 19:56:13 +0000

    part of temporary popup

--- common/components/integrations/discord/Login.tsx
+++ common/components/integrations/discord/Login.tsx
@@ -4,7 +4,7 @@ export default function Login({ clientId }: { clientId: Pointer<string> & string
   return (
     <div>
       <h1>Setup Discord Bot</h1>
-      <a href={`https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(new URL(window.location.href).origin)}%2Fintegration%2Fdiscord%2Fauth&scope=identify+guilds`}>Login</a>
+      <a href={`https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(new URL(window.location.href).origin)}%2Fintegration%2Fdiscord%2Fauth&scope=identify+guilds`} target="_blank" onclick={globalThis.close}>Login</a>
     </div>
   )
 }

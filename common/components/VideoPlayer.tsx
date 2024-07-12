be0d0d471d9e8574c113c37413f421144c05c5ce
Erfan <kamalipour.erfan@gmail.com>
2024-07-12 19:44:39 +0200

    design

--- common/components/VideoPlayer.tsx
+++ common/components/VideoPlayer.tsx
@@ -160,10 +160,21 @@ export default function VideoPlayer({ queue, code, access_token }: Readonly<{ qu
 
     
     </div>
-    <div class="rounded-full bg-white dark:bg-white/5 border border-black dark:border-white/10" > 
-    <button onclick={playNext} >
+    <div class="px-2 py-0.5 rounded-full bg-white dark:bg-white/5 border border-black dark:border-white/10 dark:text-white"> 
+    <button class="fo flex items-center ml-2 mr-1 my-1 border border-black dark:border-white/10 dark:text-white rounded-full" onclick={playNext} >
+        <svg class="mx-1.5 my-1" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+    	 width="16px" height="16px" viewBox="0 0 408.221 408.221"
+    	 xml:space="preserve">
+    <g>
+    	<g>
+    		<path d="M204.11,0C91.388,0,0,91.388,0,204.111c0,112.725,91.388,204.11,204.11,204.11c112.729,0,204.11-91.385,204.11-204.11
+    			C408.221,91.388,316.839,0,204.11,0z M286.547,229.971l-126.368,72.471c-17.003,9.75-30.781,1.763-30.781-17.834V140.012
+    			c0-19.602,13.777-27.575,30.781-17.827l126.368,72.466C303.551,204.403,303.551,220.217,286.547,229.971z"/>
+    	</g>
+    </g>
+    </svg>
       
-      play/ Next
+      <p class="mr-1">Play | Skip</p>
     </button>
     </div>
     
--- common/page.tsx
+++ common/page.tsx
@@ -130,15 +130,21 @@ export default async function App() {
             <ToggleThemeButton />
           <div class = "rounded-full bg-white dark:bg-white/5 border border-black dark:border-white/10 dark:text-white"> 
           {toggle ( unlock, 
-          
-            <p class="mb-2 font-bold">Spotify was unlocked</p>,
-          
-            <button class="fo" onclick={()=>authorizeSpotify(CLIENT_ID, CLIENT_REDIRECT, codeChallenge)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-spotify" viewBox="0 0 16 16">
+          <div class="mt-0.5 mx-2 fo flex items-center">
+            <p class="mr-1.5 mb-0.5 font-bold">Logged in</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-spotify mr-1" viewBox="0 0 16 16">
               <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.669 11.538a.5.5 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686m.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858m.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288"/>
             </svg>
-              <p class="mb-2 font-bold">Log into Spotify</p>
-            </button>
+          </div>,
+            
+          
+          <button class="mt-0.5 mx-2 fo flex items-center" onclick={() => authorizeSpotify(CLIENT_ID, CLIENT_REDIRECT, codeChallenge)}>
+            <p class="mb-2 font-bold relative -bottom-0.5">Login</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-spotify mr-2" viewBox="0 0 16 16">
+              <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.669 11.538a.5.5 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686m.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858m.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288"/>
+            </svg>
+</button>
+
            
             )}
           </div>

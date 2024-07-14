cc275ff15ae1378675851bd0462181e03aff6fe0
thaleius <58767350+thaleius@users.noreply.github.com>
2024-07-14 21:32:53 +0000

    positioning of discord login button

--- common/components/integrations/discord/Login.tsx
+++ common/components/integrations/discord/Login.tsx
@@ -2,9 +2,8 @@ import { Pointer } from "datex-core-legacy/runtime/pointers.ts";
 
 export default function Login({ clientId }: { clientId: Pointer<string> & string }) {
   return (
-    <div>
-      <h1>Setup Discord Bot</h1>
-      <a href={`https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(new URL(window.location.href).origin)}%2Fintegration%2Fdiscord%2Fauth&scope=identify+guilds`} target="_blank" onclick={globalThis.close}>Login</a>
+    <div class="w-full h-full flex items-center justify-center">
+      <a href={`https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(new URL(window.location.href).origin)}%2Fintegration%2Fdiscord%2Fauth&scope=identify+guilds`} target="_blank" onclick={globalThis.close}>Discord Login</a>
     </div>
   )
 }

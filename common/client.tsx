9091fd6e06bad8aa40cc92c3fba0a159a9d6f53d
ErfoM5 <kamalipour.erfan2@gmail.com>
2024-07-16 21:22:26 +0200

    Update client.tsx
    Dark Mode Button was enabled when light theme was active

--- common/client.tsx
+++ common/client.tsx
@@ -146,7 +146,7 @@ export default async function App(ctx: Context) {
 
           <div class="space-y-4 text-white" style={{ display: showSettings }}>
             <label class="inline-flex items-center cursor-pointer">
-              <input onclick={toggleTheme} type="checkbox" value="" class="sr-only peer"></input>
+              <input onclick={toggleTheme} type="checkbox" value="" class="sr-only peer" checked></input>
               <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
               <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark Mode</span>
             </label>
